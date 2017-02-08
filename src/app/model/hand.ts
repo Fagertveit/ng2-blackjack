ffba60988c5776e304db98d62131d38d9a5d40e1
Glenn Edberg <gridur@gmail.com>
2017-02-08 23:03:33 +0100

    Cleaning the code up some, killed a bug, ready for showtime!

--- src/app/model/hand.ts
+++ src/app/model/hand.ts
@@ -25,8 +25,8 @@ export class Hand {
       if (highValue > 21) {
         if (lowValue > 21) {
           this.isBust = true;
-          value = lowValue;
         }
+        value = lowValue;
       } else {
         value = highValue;
       }
@@ -42,33 +42,5 @@ export class Hand {
   public addCard(card: Card): void {
     this.cards.push(card);
     this.calculateHand();
-  }
-
-  public canSplit(): boolean {
-    if (this.isHouse) {
-      return false;
-    }
-
-    if (this.cards.length === 2) {
-      if (this.calculateHand() === 20) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  public split(): Hand[] {
-    let hands: Hand[];
-
-    let hand1: Hand = new Hand();
-    hand1.addCard(this.cards.pop());
-    let hand2: Hand = new Hand();
-    hand2.addCard(this.cards.pop());
-
-    hands.push(hand1);
-    hands.push(hand2);
-
-    return hands;
   }
 }
